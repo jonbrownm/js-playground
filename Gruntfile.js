64889d9a598f5571b670c467fcc2bbe663768dee
Jonathan Brown <jonathan.brown@bt.com>
2017-11-24 13:35:23 +0000

    Added basic modules for watch, scss and local server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,81 @@
 module.exports = function(grunt){
 
+	// -----------------------------------------
+	// Start Grunt configuration
+	// -----------------------------------------
+
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+
+		// Load package.json file
+		pkg: grunt.file.readJSON('package.json'),
+
+
+		// --------------------------------------
+		// Sass Configuration
+		// --------------------------------------
+
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+                    sourcemap: 'none'
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/stylesheets',
+					src: ['*.scss'],
+					dest: 'src/stylesheets',
+					ext: '.min.css'
+				}]
+			}
+		},
+
+
+		// --------------------------------------
+		// Connect Configuration
+		// --------------------------------------
+
+		watch: {
+			grunt: { files: ['Gruntfile.js'], tasks: ['default'] },
+
+			sass: {
+				files: 'src/stylesheets/**/*.scss',
+				tasks: ['sass']
+			}
+		},
+
+
+		// --------------------------------------
+		// Watch Configuration
+		// --------------------------------------
+
+		connect: {
+            all: {
+                options:{
+                    port: 8080,
+                    hostname: "0.0.0.0",
+                    base: "src",
+                    keepalive: false
+                }
+            }
+        }
+
 	});
 
-	grunt.registerTask('default', []);
+
+	// -----------------------------------------
+	// Load Grunt tasks
+	// -----------------------------------------
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+	// -----------------------------------------
+	// Register Grunt tasks
+	// -----------------------------------------
+
+	grunt.registerTask('default',  ['sass','connect','watch']);
 
 };
