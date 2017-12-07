3aaeda2671caf31ece18146ae88b0a26745d4880
Jonathan Brown <jonathan.brown@bt.com>
2017-12-07 11:58:59 +0000

    Updates to be consistent

--- src/js/components/genericComponent.js
+++ src/js/components/genericComponent.js
@@ -1,5 +1,7 @@
-import {functionA} from './genericComponent/functionA'
-import {functionB} from './genericComponent/functionB'
+import {functionA} from './genericComponent/functionA';
+import {functionB} from './genericComponent/functionB';
+
+const genericComponent = document.querySelector("div#genericComponent");
 
 export const genericComponent = () => {
 
