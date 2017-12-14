a83654adca0e35d27ae14807a77b7e507d71ce55
Jonathan Brown <jonathan.brown@bt.com>
2017-12-14 09:24:11 +0000

    Changed name and initiation of js components

--- src/js/components/genericComponent.js
+++ src/js/components/genericComponent.js
@@ -1,7 +1,7 @@
 import {functionA} from './genericComponent/functionA';
 import {functionB} from './genericComponent/functionB';
 
-const genericComponentElement = document.querySelector("div#genericComponent");
+const genericComponentElement = document.querySelector("[data-component-name='genericComponent']");
 
 export const genericComponent = () => {
 
