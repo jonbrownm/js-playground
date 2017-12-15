b1c08329c6dba6f7d5d3c67d26becfddd6e6de64
Jonathan Brown <jonathan.brown@bt.com>
2017-12-15 14:57:25 +0000

    Added click handler for load all

--- src/js/components/cmpCategoryArticleSelector.js
+++ src/js/components/cmpCategoryArticleSelector.js
@@ -1,9 +1,11 @@
 import {getSelectedCategory} from './cmpCategoryArticleSelector/getSelectedCategory';
+import {showAllSelectedCategory} from './cmpCategoryArticleSelector/showAllSelectedCategory';
 
 const genericComponentElement = document.querySelector("[data-component-name='cmpCategoryArticleSelector']");
 
 export const cmpCategoryArticleSelector = () => {
 
 	getSelectedCategory();
+	showAllSelectedCategory();
 	
 }
