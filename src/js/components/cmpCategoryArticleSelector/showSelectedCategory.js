fd2ca9cdb1ea0094d37a473151bf27e2f2b1c62a
Jonathan Brown <jonathan.brown@bt.com>
2017-12-15 12:46:45 +0000

    Developed function to hide and show based on click

--- src/js/components/cmpCategoryArticleSelector/showSelectedCategory.js
+++ src/js/components/cmpCategoryArticleSelector/showSelectedCategory.js
@@ -1,12 +1,19 @@
 
+var elementArticleItems = [].slice.call(document.querySelectorAll('.cmp-category-article-selector__article-list-item')),
+	elementArticleItemCategory;
+
 export const showSelectedCategory = (currentSelectedCategory) => {
 	console.log(currentSelectedCategory);
 
 	if (currentSelectedCategory === 'All') {
-		console.log('all selected')
+		elementArticleItems.forEach(function(elementArticleItem) {
+		    elementArticleItem.style.display = 'block';
+		});
 	}
 	else {
-		console.log('the rest')		
+		elementArticleItems.forEach(function(elementArticleItem) {
+		    elementArticleItem.style.display = 'none';
+		});
 	}
 
 }
