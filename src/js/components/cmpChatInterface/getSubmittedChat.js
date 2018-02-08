a372c9e274daada3f4ba3ac71c05142c3c3836a7
Jonathan Brown <jonathan.brown@bt.com>
2018-02-08 12:05:22 +0000

    Removed text after response has been submitted

--- src/js/components/cmpChatInterface/getSubmittedChat.js
+++ src/js/components/cmpChatInterface/getSubmittedChat.js
@@ -6,8 +6,14 @@ export const getSubmittedChat = (elementSubmitButton) => {
 	elementSubmitButton.addEventListener('keyup', function (e) {
 
 	    if (e.keyCode === 13) {
-	        var submittedChat = document.querySelector("[data-new-chat]").value;
-	        showSubmittedChat(submittedChat);
+
+	    	var submittedChat = document.querySelector("[data-new-chat]"),
+	    		submittedChatMessage = document.querySelector("[data-new-chat]").value;
+
+
+	        submittedChat.value = "";
+	        showSubmittedChat(submittedChatMessage);
+	        
 	    }
 
 	});
