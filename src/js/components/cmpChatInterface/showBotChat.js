bdda600627e6bf658135be21a6ab77047b67c393
Jonathan Brown <jonathan.brown@bt.com>
2018-02-08 12:31:25 +0000

    Added chat bot responses

--- src/js/components/cmpChatInterface/showBotChat.js
+++ src/js/components/cmpChatInterface/showBotChat.js
@@ -3,14 +3,20 @@ export const showBotChat = () => {
 
 	var chatList = document.querySelector("[data-chat-list]"),
 		newChatListItem = document.createElement("li"),
-		newChatListSpan = document.createElement("span");
+		newChatListSpan = document.createElement("span"),
+        chatResponseIndex = Math.floor((Math.random() * 2) + 1),
+        chatResponses = [
+            "Sorry, I don't understand.",
+            "Hello, is anyone there?",
+            "Ok, I see what you mean."
+        ];
 
 
     newChatListItem.classList.add("cmp-chat-interface__dialogue-list-item");
     newChatListItem.classList.add("is-bot");
     newChatListSpan.classList.add("cmp-chat-interface__dialogue-list-item-chat");
-    
-    newChatListSpan.innerText = "What?";
+
+    newChatListSpan.innerText = chatResponses[chatResponseIndex];
 
     newChatListItem.appendChild(newChatListSpan);
 
