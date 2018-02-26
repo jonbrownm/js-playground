929ccd9909b2ab5d2550bb6ac1f377457689a9ec
Jonathan Brown <jonathan.brown@bt.com>
2018-02-26 10:04:19 +0000

    Removed load all button when all articles are loaded

--- src/js/components/cmpCategoryArticleSelector/showAllSelectedCategory.js
+++ src/js/components/cmpCategoryArticleSelector/showAllSelectedCategory.js
@@ -1,6 +1,8 @@
 
 export const showAllSelectedCategory = (currentViewArticleList, allArticles) => {
 
+	const elementLoadAllArticles= document.querySelector('.cmp-category-article-selector__article-load-container-button');
+
 	if (allArticles) {
 		currentViewArticleList.forEach(function(article, i){
 			article.style.display = "block";
@@ -8,6 +10,7 @@ export const showAllSelectedCategory = (currentViewArticleList, allArticles) => 
 		// if (currentViewArticleList.length > 6) {
 		// 	filterLoad.style.display = "none";
 		// }
+		elementLoadAllArticles.style.display = "none";
 	}
 	else if (!allArticles) {
 		currentViewArticleList.forEach(function(article, i){
@@ -18,6 +21,7 @@ export const showAllSelectedCategory = (currentViewArticleList, allArticles) => 
 		// if (currentViewArticleList.length > 6) {
 		// 	filterLoad.style.display = "block";
 		// }
+		elementLoadAllArticles.style.display = "block";
 	}
 
 }
