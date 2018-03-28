cd60e9f141875aebe16ad80d2bcc934acde79ae7
Jonathan Brown <jonathan.brown@bt.com>
2018-03-28 13:34:04 +0100

    Added new styling

--- src/js/components/cmpCountrySearch/returnCountryResults.js
+++ src/js/components/cmpCountrySearch/returnCountryResults.js
@@ -3,17 +3,34 @@
 
 export const returnCountryResults = (countryList, userSearchInput) => {
 
-	var currentCountry,
+	var elementSearchList = document.querySelector("[data-component-name='cmpCountrySearch'] ul"),
+		searchResultsDisplayed = 5,
+		searchResultsDisplayedCurrent,
+		currentCountry,
 		index;
-	
 
-	for (index = 0; index < countryList.length; index++) {
-		currentCountry = countryList[index].toLowerCase();
 
-		if (currentCountry.match("^" + userSearchInput)) {
-			console.log(currentCountry)
+	if (userSearchInput) {
+		elementSearchList.innerHTML = "";
+
+		for (index = 0; index < countryList.length; index++) {
+
+			var newListItem = document.createElement("li");
+			
+
+			currentCountry = countryList[index].toLowerCase();
+
+			if (currentCountry.match("^" + userSearchInput)) {
+				newListItem.className = "cmp-country-search__results-item";
+				newListItem.appendChild(document.createTextNode(currentCountry));
+				elementSearchList.appendChild(newListItem);
+				searchResultsDisplayedCurrent = elementSearchList.childElementCount; 
+			}
 		}
-	}
 
+	}
+	else {
+		elementSearchList.innerHTML = "";
+	}
 
 }
