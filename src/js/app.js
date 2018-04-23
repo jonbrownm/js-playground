976818735e2e9b5c78d2d6ec8d6133edd1c77be3
Jonathan Brown <jonathan.brown@bt.com>
2018-04-23 10:44:08 +0100

    Removed carousel

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,7 @@
 import {cmpCategoryArticleSelector} from './components/cmpCategoryArticleSelector';
 import {cmpCountrySearch} from './components/cmpCountrySearch';
 import {cmpCardSelector} from './components/cmpCardSelector';
-import {cmpGalleryCarousel} from './components/cmpGalleryCarousel';
 
 cmpCategoryArticleSelector();
 cmpCountrySearch();
 cmpCardSelector();
-cmpGalleryCarousel();
